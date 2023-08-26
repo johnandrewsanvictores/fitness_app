2a233282db0d836cd09343df0827fd9b1fa9ccd8
johnandrewsanvictores <sanvictoresjohnandrewe@gmail.com>
2023-08-26 12:54:50 +0800

    finished search exercises

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,29 @@
+import { useState } from "react";
+import { Box } from "@mui/material";
+
+import HeroBanner from '../components/HeroBanner';
+import SearchExercises from '../components/SearchExercises';
+import Excercises from '../components/Exercises';
+
+
 const Home = () => {
+    const [exercises, setExercises] = useState([]);
+    const [bodyPart, setBodyPart] = useState('all');
+
     return ( 
-        <div>Home</div>
+        <Box>
+            <HeroBanner />
+            <SearchExercises 
+                setExercises={setExercises} 
+                bodyPart={bodyPart}
+                setBodyPart={setBodyPart} 
+            />
+            <Excercises 
+                setExercises={setExercises} 
+                bodyPart={bodyPart}
+                setBodyPart={setBodyPart} 
+            />
+        </Box>
      );
 }
  
