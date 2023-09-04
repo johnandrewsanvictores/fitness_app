aae7e4af391ddb8d4a21b47f15fb2207514f614c
johnandrewsanvictores <sanvictoresjohnandrewe@gmail.com>
2023-09-04 20:38:55 +0800

    update recommended video section

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,52 +1,105 @@
 import { Typography, Stack, Button } from "@mui/material";
 
-import BodyPartImage from '../assets/icons/body-part.png';
-import TargetImage from '../assets/icons/target.png';
-import EquipmentImage from '../assets/icons/equipment.png';
-
+import BodyPartImage from "../assets/icons/body-part.png";
+import TargetImage from "../assets/icons/target.png";
+import EquipmentImage from "../assets/icons/equipment.png";
 
 const Detail = ({ exerciseDetail }) => {
+  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
 
-    const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+  const extraDetail = [
+    {
+      icon: BodyPartImage,
+      name: bodyPart,
+    },
+    {
+      icon: TargetImage,
+      name: target,
+    },
+    {
+      icon: EquipmentImage,
+      name: equipment,
+    },
+  ];
 
-    const extraDetail = [
-        {
-            icon: BodyPartImage,
-            name: bodyPart,
-        },
-        {
-            icon: TargetImage,
-            name: target,
-        },
-        {
-            icon: EquipmentImage,
-            name: equipment,
-        }
-    ]
+  return (
+    <div className="flex flex-col gap-9 p-5 items-center justify-center md:flex-row md:px-12 md:py-24 ">
+      <img
+        src={gifUrl}
+        alt={name}
+        loading="lazy"
+        className=" w-full max-w-[35rem] h-auto  bg-black rounded-lg"
+      />
+      <div>
+        <h3 className="text-red-500 text-3xl leading-8 capitalize md:text-5xl">
+          {name}
+        </h3>
+        <h6 className="text-gray-900 text-base max-w-[56ch] my-4">
+          Exercises keep you strong. {name} is one of the best exercises to
+          target your {target}. It will help you improve your mood and gian
+          energy.
+        </h6>
 
-    return ( 
-        <Stack gap="60px" sx={{flexDirection: { lg: 'row'}, p: '20px', alignItems: 'center'}}>
-            <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
-            <Stack sx={{gap: { lg: '35px', xs: '20px'}}}>
-                <Typography variant="h3">
-                    {name}
-                </Typography>
-                <Typography variant="h6">
-                    Exercises keep you strong. {name} is one of the best exercises to target your {target}. It will help you improve your mood and gain energy.
-                </Typography>
-                {extraDetail.map((item) => (
-                    <Stack key={item.name} direction="row" gap="24px" alignItems="center">
-                        <Button sx={{ background: "#fff2db", borderRadius: '50%', width: '100px', height: '100px'}}>
-                            <img src={item.icon} alt={bodyPart} style={{ width: '50px', height: '50px'}} />
-                        </Button>
-                        <Typography textTransform="capitalize" variant="h5">
-                            {item.name}
-                        </Typography>
-                    </Stack>
-                ))}
-            </Stack>
-        </Stack>
-     );
-}
- 
+        <div className="flex gap-8 flex-col mt-6">
+          {extraDetail.map((item) => (
+            <div className="flex gap-2 items-center ">
+              <button>
+                <img
+                  src={item.icon}
+                  alt={bodyPart}
+                  className=" max-w-3xl h-auto aspect-square"
+                />
+              </button>
+              <h5 className="text-base text-gray-900 capitalize">
+                {item.name}
+              </h5>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+
+    // <Stack
+    //   gap="60px"
+    //   sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
+    // >
+    //   <img
+    //     src={gifUrl}
+    //     alt={name}
+    //     loading="lazy"
+    //     className=" w-full max-w-[35rem] h-auto  bg-black"
+    //   />
+    //   <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
+    //     <Typography variant="h3">{name}</Typography>
+    //     <Typography variant="h6">
+    //       Exercises keep you strong. {name} is one of the best exercises to
+    //       target your {target}. It will help you improve your mood and gain
+    //       energy.
+    //     </Typography>
+    //     {extraDetail.map((item) => (
+    //       <Stack key={item.name} direction="row" gap="24px" alignItems="center">
+    //         <Button
+    //           sx={{
+    //             background: "#fff2db",
+    //             borderRadius: "50%",
+    //             width: "100px",
+    //             height: "100px",
+    //           }}
+    //         >
+    //           <img
+    //             src={item.icon}
+    //             alt={bodyPart}
+    //             style={{ width: "50px", height: "50px" }}
+    //           />
+    //         </Button>
+    //         <Typography textTransform="capitalize" variant="h5">
+    //           {item.name}
+    //         </Typography>
+    //       </Stack>
+    //     ))}
+    //   </Stack>
+    // </Stack>
+  );
+};
+
 export default Detail;
--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,43 +1,71 @@
 import { Box, Stack, Typography } from "@mui/material";
 
-const ExerciseVideos = ({ exerciseVideos, name}) => {
+const ExerciseVideos = ({ exerciseVideos, name }) => {
+  if (!exerciseVideos.length) return "Loading...";
 
-    if(!exerciseVideos.length) return 'Loading...';
+  return (
+    <div className=" my-12 flex flex-col gap-8 items-center p-5 md:px-12 md:py-24 md:pt-1">
+      <h3 className="self-start text-xl md:text-3xl text-gray-800">
+        {" "}
+        Watch <span className=" text-red-500 capitalize">{name}</span> exercise
+        videos
+      </h3>
+      <div className="flex flex-row justify-center items-center flex-wrap gap-16">
+        {exerciseVideos?.slice(0, 4).map((item, index) => (
+          <a
+            key={index}
+            className="flex flex-col gap-4 items-center justify-center w-full max-w-[25em]  h-auto"
+            href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+            target="__blank"
+            rel="noreferrer"
+          >
+            <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+            <div>
+              <h5 className="text-center text-[#292929] text-base leading-8">
+                {item.video.title}
+              </h5>
+              <h6 className="text-center text-[#5c5c5c] text-sm">
+                {item.video.channelName}
+              </h6>
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
 
-    return ( 
-        <Box sx={{ marginTop: {lg: '200px', xs: '20px' }}} p="20px">
-            <Typography variant="h3" mb="33px">
-                Watch <span style={{color: '#ff2625', textTransform: 'capitalize'}}>{name}</span> exercise videos
-            </Typography>
-            <Stack justifyContent="flex-start" flexWrap="wrap" alignItems="center" sx={{
-                flexDirection: {lg:'row'},
-                gap: {lg: '110px', xs: '0'}
-            }}>
-                
-                {
-                    exerciseVideos?.slice(0,4).map((item, index) => (
-                        <a
-                            key={index}
-                            className="exercise-video"
-                            href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
-                            target="__blank"
-                            rel="noreferrer"
-                        >
-                            <img src={item.video.thumbnails[0].url} alt={item.video.title} />
-                            <Box>
-                                <Typography variant="h5" color="#121212">
-                                    {item.video.title}
-                                </Typography>
-                                <Typography variant="h6" color="#121212">
-                                    {item.video.channelName}
-                                </Typography>
-                            </Box>
-                        </a>
-                    ))
-                }
-            </Stack>
-        </Box>
-     );
-}
- 
+    // <Box sx={{ marginTop: {lg: '200px', xs: '20px' }}} p="20px">
+    //     <Typography variant="h3" mb="33px">
+    //         Watch <span style={{color: '#ff2625', textTransform: 'capitalize'}}>{name}</span> exercise videos
+    //     </Typography>
+    //     <Stack justifyContent="flex-start" flexWrap="wrap" alignItems="center" sx={{
+    //         flexDirection: {lg:'row'},
+    //         gap: {lg: '110px', xs: '0'}
+    //     }}>
+
+    //         {
+    //             exerciseVideos?.slice(0,4).map((item, index) => (
+    //                 <a
+    //                     key={index}
+    //                     className="exercise-video"
+    //                     href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+    //                     target="__blank"
+    //                     rel="noreferrer"
+    //                 >
+    //                     <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+    //                     <Box>
+    //                         <Typography variant="h5" color="#121212">
+    //                             {item.video.title}
+    //                         </Typography>
+    //                         <Typography variant="h6" color="#121212">
+    //                             {item.video.channelName}
+    //                         </Typography>
+    //                     </Box>
+    //                 </a>
+    //             ))
+    //         }
+    //     </Stack>
+    // </Box>
+  );
+};
+
 export default ExerciseVideos;
